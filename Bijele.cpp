fe1b46f69cc167815f8b817042cf14e0a4dbfc5c
atinafabebe1 <109695272+atinafabebe1@users.noreply.github.com>
2022-07-21 20:53:25 +0300

    new

---
+++ Bijele.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, d, e, f, x, y, z;
+    const int g = 1, h = 1, i = 2, j = 2, k = 2, l = 8;
+
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << g - a << " " << h - b <<" "<< i - c << " " << j - d << " " << k - e << " " << l - f;
+}
